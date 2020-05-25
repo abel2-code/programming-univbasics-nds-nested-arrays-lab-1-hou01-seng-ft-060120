44b2b6fada5d5a76c489d019993cb83f2e641105
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 03:17:40 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -47,7 +47,7 @@ def matrix_update(matrix, row, column, new_value)
   else
     sorted_matrix[row][column] = new_value
   end
-  matrix[row][column][new_value]
+  matrix
   # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
