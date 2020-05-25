5319cfbec40ede91051925e39bd8b306ac813d10
Abelardo Miranda <available_email_address@yahoo.com>
2020-05-25 03:07:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,6 +35,7 @@ def matrix_lookup(matrix, row, column)
     assembled_matrix[row][column]
   else
     sorted_matrix[row][column]
+  end
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
